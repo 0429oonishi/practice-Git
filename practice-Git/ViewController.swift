c859339130531dc775492e1b0582b2881e8bf6ec
0429oonishi <oonishireon110@gmail.com>
2021-03-25 08:58:23 +0900

    消去

--- practice-Git/ViewController.swift
+++ practice-Git/ViewController.swift
@@ -5,20 +5,4 @@
 //  Created by 大西玲音 on 2021/03/25.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("issue1")
-        print("issue2")
-        print("issue2")
-        print("issue2")
-        print("issue2")
-        print("issue2")
-    }
-
-
-}
 
