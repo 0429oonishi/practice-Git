468631bdf5506e9a922fe6e3ad077e49d5fa5196
0429oonishi <oonishireon110@gmail.com>
2021-03-25 09:07:00 +0900

    aaaa

--- practice-Git/ViewController.swift
+++ practice-Git/ViewController.swift
@@ -5,4 +5,25 @@
 //  Created by 大西玲音 on 2021/03/25.
 //
 
+<<<<<<< HEAD
+=======
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("issue1")
+        print("issue3")
+        print("issue4")
+        print("issue4")
+        print("issue4")
+        print("issue4")
+        print("issue4")
+        print("issue4")
+    }
+
+
+}
+>>>>>>> 45f7ff5... aaaa
 
